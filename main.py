360f3c4ea84458e0fdc782c59ca7cfbbf9047ba0
nikita <nikpritter@yandex.ru>
2026-08-14 22:42:09 +0300

    Почистил код Убрал лишнюю переменную(stop) и изменил цикл-затычку

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys #Импорт необходимой билиотеки
 pygame.init() #Запуск библиотеки pygame
 
 
-stop = False # Переменная для цикла-затычки
 width = 800 # Ширина окна
 height = 600 # Высота окна
 color = (255,255,255) # Цвет
@@ -16,7 +15,7 @@ def Main():
     screen.fill(color) # Заливка цвета
     pygame.display.flip() # Обновление окна
 
-    while stop != True: #Цикл-затычка чтобы не сворачивалось окно
+    while True: #Цикл-затычка чтобы не сворачивалось окно
         for event in pygame.event.get(): # Запись каждого нажатия в переменную event
             if event.type == pygame.QUIT: # Проверка кнопки
                 sys.exit() # Выход из окна

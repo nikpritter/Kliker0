4dd2c0e8d4f52a25ec074fec7c03fcba9cd11cad
nikita <nikpritter@yandex.ru>
2026-08-15 00:00:42 +0300

    Добавлен счёт валюты Добавленна переменная для хранения валюты Добавлена опция начиления валюты Добавлены комментарии к новым строкам

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame #Импорт необходимой билиотеки pygame
 import sys #Импорт необходимой билиотеки
+import functions #Импорт файла с функциями
 
 
 pygame.init() #Запуск библиотеки pygame
@@ -7,8 +8,12 @@ pygame.init() #Запуск библиотеки pygame
 
 width = 800 # Ширина окна
 height = 600 # Высота окна
+
 color = (255,255,255) # Белый цвет
 red = (255,0,0) # Красный цвет
+
+score = 0 # Счёт игровой валюты
+
 
 def Main():
     screen = pygame.display.set_mode((width, height)) #Запуск экрана через библиотеку pygame
@@ -16,11 +21,13 @@ def Main():
     screen.fill(color) # Заливка цвета
     pygame.display.flip() # Обновление окна
 
+    global score # Объявление глобальной переменной
+
     while True:  # Цикл-затычка чтобы не сворачивалось окно
         for event in pygame.event.get(): # Запись каждого нажатия в переменную event
             if event.type == pygame.MOUSEBUTTONDOWN: # Проверка на нажатие кнопки мыши
                 if event.button == 1: # Проверка на нажатие левой кнопки мыши
-                    print(1)
+                    score += 1 # Начисление валюты за клик
 
 
 
@@ -28,12 +35,8 @@ def Main():
                 screen.fill(red) # Покраска в красный
                 pygame.display.flip()  # Обновление окна
                 pygame.time.delay(100) # Задержка на 0,1 секунды
+                print(score) # Вывод итогового результата
                 sys.exit() # Выход из окна
-
-
-
-
-
 
 
 Main()

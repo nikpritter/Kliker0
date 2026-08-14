b51d2083e915affee8a494a6ea1153b529325121
nikita <nikpritter@yandex.ru>
2026-08-14 23:34:57 +0300

    Добавил покраснение окна перед закытием Добавил комментарии к новым строкам

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ pygame.init() #Запуск библиотеки pygame
 
 width = 800 # Ширина окна
 height = 600 # Высота окна
-color = (255,255,255) # Цвет
+color = (255,255,255) # Белый цвет
+red = (255,0,0) # Красный цвет
 
 def Main():
     screen = pygame.display.set_mode((width, height)) #Запуск экрана через библиотеку pygame
@@ -15,9 +16,12 @@ def Main():
     screen.fill(color) # Заливка цвета
     pygame.display.flip() # Обновление окна
 
-    while True: #Цикл-затычка чтобы не сворачивалось окно
+    while True:  # Цикл-затычка чтобы не сворачивалось окно
         for event in pygame.event.get(): # Запись каждого нажатия в переменную event
             if event.type == pygame.QUIT: # Проверка кнопки
+                screen.fill(red) # Покраска в красный
+                pygame.display.flip()  # Обновление окна
+                pygame.time.delay(100) # Задержка на 0,1 секунды
                 sys.exit() # Выход из окна
 
 

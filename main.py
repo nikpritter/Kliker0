84ff7a11f9a2134714f19cc69773341ec4fe102b
nikita <nikpritter@yandex.ru>
2026-08-14 20:05:58 +0300

    Переименовал окно в "Kliker0" и добавил комментарий к этой строчке

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ pygame.init() #Запуск библиотеки pygame
 
 stop = False
 screen = pygame.display.set_mode((1000, 500)) #Запуск экрана через библиотеку pygame
+pygame.display.set_caption("Kliker0") #Присвоение окну названия "Kliker0"
 
 while stop != True: #Цикл-затычка чтобы не сворачивалось окно
     a = 1

48e08a69851a9f28bc56af432acad4e953c47e95
nikita <nikpritter@yandex.ru>
2026-08-14 21:39:36 +0300

    Добавил для окна цвет Добавил к новым строчкам комментарии

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ pygame.init() #Запуск библиотеки pygame
 stop = False # Переменная для цикла-затычки
 width = 800 # Ширина окна
 height = 600 # Высота окна
+color = (255,255,255) # Цвет
 
 def Main():
     screen = pygame.display.set_mode((width, height)) #Запуск экрана через библиотеку pygame
     pygame.display.set_caption("Kliker0") #Присвоение окну названия "Kliker0"
+    screen.fill(color) # Заливка цвета
+    pygame.display.flip() # Обновление окна
 
     while stop != True: #Цикл-затычка чтобы не сворачивалось окно
         for event in pygame.event.get(): # Запись каждого нажатия в переменную event

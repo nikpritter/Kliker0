ae0747c98bc799ddaa23eb08d3c3abbc8f89c6c9
nikita <nikpritter@yandex.ru>
2026-08-14 20:39:27 +0300

    Перенёс основную программу в функцию main() Добавил переменные ширины и высоты для окна Добавил недостающие комментарии

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame #Импорт необходимой билиотеки pygame
+
+
 pygame.init() #Запуск библиотеки pygame
 
-stop = False
-screen = pygame.display.set_mode((1000, 500)) #Запуск экрана через библиотеку pygame
-pygame.display.set_caption("Kliker0") #Присвоение окну названия "Kliker0"
 
-while stop != True: #Цикл-затычка чтобы не сворачивалось окно
-    a = 1
+stop = False # Переменная для цикла-затычки
+width = 0 # Ширина окна
+height = 0 # Высота окна
+
+def main():
+    screen = pygame.display.set_mode((width, height)) #Запуск экрана через библиотеку pygame
+    pygame.display.set_caption("Kliker0") #Присвоение окну названия "Kliker0"
+
+    while stop != True: #Цикл-затычка чтобы не сворачивалось окно
+        a = 1
+
+
+main()

6a0665b2ece98910d5e17c4cf134c4de3d99b6a6
nikita <nikpritter@yandex.ru>
2026-08-15 22:19:34 +0300

    Версия 0.1.3 Оптимизация кода

--- functions.py
+++ functions.py
@@ -7,17 +7,21 @@ import sys
 
 pygame.init() #Запуск библиотеки pygame
 
+def frame_rendering(screen):
+    screen.fill(variables.white)  # Покраска в белый
+    text_surface = variables.font.render(str(variables.score), True, variables.red)  # Рендер значения счёта в картинку
+    screen.blit(text_surface, (20, 10))  # Вывод картинки со счётом на экран
+    pygame.draw.rect(screen, variables.white, variables.button_klik)  # Заливка кнопки
+    pygame.draw.rect(screen, variables.red, variables.button_klik, 2)  # Отрисовка границы кнопки
+    screen.blit(variables.button_text,(variables.button_klik.x + 40, variables.button_klik.y + 14))  # Добавление текста на кнопку
+    pygame.display.flip()  # Обновление окна
+
 def main(): # Создание основной функции
 
     screen = pygame.display.set_mode((variables.width, variables.height)) #Запуск экрана через библиотеку pygame
     pygame.display.set_caption("Kliker0") #Присвоение окну названия "Kliker0"
-    screen.fill(variables.white) # Покраска в белый
-    text_surface = variables.font.render(str(variables.score), True, variables.red)  # Рендер значения счёта в картинку
-    screen.blit(text_surface, (20, 10))  # Вывод картинки со счётом на экран
-    pygame.draw.rect(screen, variables.white, variables.button_klik) # Заливка кнопки
-    pygame.draw.rect(screen, variables.red, variables.button_klik, 2) # Отрисовка границы кнопки
-    screen.blit(variables.button_text, (variables.button_klik.x + 40, variables.button_klik.y + 14)) # Добавление текста на кнопку
-    pygame.display.flip() # Обновление окна
+
+    frame_rendering(screen)
 
     while True:  # Рабочий цикл
         for event in pygame.event.get(): # Запись каждого нажатия в переменную event
@@ -29,22 +33,10 @@ def main(): # Создание основной функции
 
 
 
-                    screen.fill(variables.white)  # Покраска в белый
-                    text_surface = variables.font.render(str(variables.score), True, variables.red)  # Рендер значения счёта в картинку
-                    screen.blit(text_surface, (20, 10))  # Вывод картинки со счётом на экран
-                    pygame.draw.rect(screen, variables.white, variables.button_klik) # Заливка кнопки
-                    pygame.draw.rect(screen, variables.red, variables.button_klik, 2) # Отрисовка границы кнопки
-                    screen.blit(variables.button_text, (variables.button_klik.x + 40, variables.button_klik.y + 14)) # Добавление текста на кнопку
-                    pygame.display.flip()  # Обновление окна
-
-
-
+                    frame_rendering(screen)
             if event.type == pygame.QUIT: # Проверка кнопки
                 screen.fill(variables.red) # Покраска в красный
                 pygame.display.flip()  # Обновление окна
                 pygame.time.delay(100) # Задержка на 0,1 секунды
                 print(variables.score) # Вывод итогового результата
                 sys.exit() # Выход из окна
-
-
-

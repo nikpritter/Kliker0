47d96fad7959b42b05d867d0387984ed7cfb475e
nikita <nikpritter@yandex.ru>
2026-08-15 17:51:07 +0300

    Почистил код Добавил отображение нуля при отсутствии кликов

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pygame #Импорт необходимой билиотеки pygame
-import sys #Импорт необходимой билиотеки
-import functions #Импорт файла с функциями
+import pygame # Импорт необходимой билиотеки pygame
+import sys # Импорт необходимой билиотеки
+#import functions # Импорт файла с функциями(на будущее)
 
 
 pygame.init() #Запуск библиотеки pygame
@@ -19,12 +19,15 @@ score = 0 # Счёт игровой валюты
 
 
 def main(): # Создание основной функции
+
+    global score # Объявление глобальной переменной
+
     screen = pygame.display.set_mode((width, height)) #Запуск экрана через библиотеку pygame
     pygame.display.set_caption("Kliker0") #Присвоение окну названия "Kliker0"
     screen.fill(white) # Покраска в белый
+    text_surface = font.render(str(score), True, red)  # Рендер значения счёта в картинку
+    screen.blit(text_surface, (20, 10))  # Вывод картинки со счётом на экран
     pygame.display.flip() # Обновление окна
-
-    global score # Объявление глобальной переменной
 
     while True:  # Цикл-затычка чтобы не сворачивалось окно
         for event in pygame.event.get(): # Запись каждого нажатия в переменную event
